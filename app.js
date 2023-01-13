f9603aa7c004f3f02153660fcc9321f180a8f360
Ayush-dvplr <ayush.dvlpr@gmail.com>
2023-01-13 23:07:33 +0530

    repushed arpit code

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+
+// const cors = require("cors");
+const app = express();
+
+// Routes Imports
+const Product = require("./routes/productRoute");
+const User = require("./routes/userRoutes");
+const order = require("./routes/orderRoute");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(cookieParser());
+app.use("/api/vi", Product);
+app.use("/api/vi", User);
+app.use("/api/vi", order);
+
+// Middleware for error
+app.use(errorMiddleware);
+
+module.exports = app;
